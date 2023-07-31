051ccc5ca0ff05c0e5655a42ea6cd2025a3ed720
Harvey Kwak <harvey970121@naver.com>
2023-07-31 16:11:35 +0900

    :art: ItemResponseDto 완성
    ItemResponseDto 완성
    Resolves: #42

--- src/main/java/com/b5f1/atention/domain/item/dto/ItemResponseDto.java
+++ src/main/java/com/b5f1/atention/domain/item/dto/ItemResponseDto.java
@@ -1,4 +1,18 @@
 package com.b5f1.atention.domain.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemResponseDto {
+    // 아이템 이름
+    private String name;
+    // 아이템 이미지
+    private String itemImage;
+    // 아이템 설명
+    private String description;
 }
