b9e07049386e7a33af45843564b6f1b823acc1fb
novventa <novventa@kakao.com>
2023-08-01 16:32:28 +0900

    :recycle:Plan 생성 및 상태 변경 시 권한 확인 로직 추가 권한 확인 로직 추가를 위해  PlanService 및 PlanRepository, DTO 수정 Resolves #54

--- src/main/java/com/b5f1/atention/domain/plan/repository/PlanRepository.java
+++ src/main/java/com/b5f1/atention/domain/plan/repository/PlanRepository.java
@@ -6,13 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-
-    // userId로 해당 유저의 모든 일정 가져오기
-    List<Plan> findAllByUserIdAndIsDeletedFalse(UUID userId);
 
     // teamId로 해당 팀의 모든 일정 가져오기
     List<Plan> findAllByTeamIdAndIsDeletedFalse(Long teamId);
--- src/main/java/com/b5f1/atention/domain/plan/service/PlanService.java
+++ src/main/java/com/b5f1/atention/domain/plan/service/PlanService.java
@@ -2,24 +2,19 @@ package com.b5f1.atention.domain.plan.service;
 
 import com.b5f1.atention.domain.plan.dto.PlanRequestDto;
 import com.b5f1.atention.domain.plan.dto.PlanResponseDto;
-import com.b5f1.atention.entity.Plan;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PlanService {
 
-    // 모든 일정 가져오기
-    List<PlanResponseDto> getAllPlans();
-    // 유저 ID로 일정 가져오기
-    List<PlanResponseDto> getPlansByUserId(UUID userId);
     // 팀 ID로 일정 가져오기
-    List<PlanResponseDto> getPlansByTeamId(Long teamId);
+    List<PlanResponseDto> getAllTeamPlans(UUID userId, Long teamId);
     // 일정 생성
-    PlanResponseDto createPlan(PlanRequestDto planRequestDto);
+    PlanResponseDto createPlan(UUID userId, PlanRequestDto planRequestDto);
     // 일정 수정
-    PlanResponseDto updatePlan(Long planId, PlanRequestDto planRequestDto);
+    PlanResponseDto updatePlan(UUID userId, Long planId, PlanRequestDto planRequestDto);
     // 일정 삭제
-    void deletePlan(Long planId);
+    void deletePlan(UUID userId, Long planId, PlanRequestDto planRequestDto);
 
 }
