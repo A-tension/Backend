0164da51e31246350c7f5e2fcadb64b646868392
novventa <novventa@kakao.com>
2023-08-02 11:18:29 +0900

    :construction: PlanController ResponseEntity MessageWithData로 통일 PlanController ResponseEntity MessageWithData로 통일, 임시 코드임 Resolves #47

--- src/main/java/com/b5f1/atention/domain/plan/controller/PlanController.java
+++ src/main/java/com/b5f1/atention/domain/plan/controller/PlanController.java
@@ -1,8 +1,10 @@
 package com.b5f1.atention.domain.plan.controller;
 
+import com.b5f1.atention.common.MessageWithData;
 import com.b5f1.atention.domain.plan.dto.PlanRequestDto;
 import com.b5f1.atention.domain.plan.dto.PlanResponseDto;
 import com.b5f1.atention.domain.plan.service.PlanService;
+import com.b5f1.atention.entity.Plan;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -22,33 +25,45 @@ public class PlanController {
 
     private final PlanService planService;
 
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<PlanResponseDto>> getAllUserPlans(@PathVariable UUID userId) {
-        List<PlanResponseDto> plans = planService.getAllPlans(userId);
-        return ResponseEntity.ok(plans);
+    @GetMapping("/{userId}")
+    public ResponseEntity<MessageWithData> getAllUserPlans(@PathVariable UUID userId) {
+//        List<PlanResponseDto> plans = planService.getAllPlans(userId);
+//        return ResponseEntity.ok(plans);
+        return null;
     }
 
-    @GetMapping("/team/{teamId}")
-    public ResponseEntity<List<PlanResponseDto>> getAllTeamPlans(@PathVariable Long teamId) {
-        List<PlanResponseDto> plans = planService.getAllTeamPlans(teamId);
-        return ResponseEntity.ok(plans);
+    @GetMapping("/{teamId}")
+    public ResponseEntity<MessageWithData> getAllTeamPlans(@PathVariable Long teamId) {
+//        List<PlanResponseDto> plans = planService.getAllTeamPlans(teamId);
+//        return ResponseEntity.ok(plans);
+        return null;
     }
 
-    @PostMapping("/user/{userId}")
-    public ResponseEntity<PlanResponseDto> createPlan(@PathVariable UUID userId, @RequestBody PlanRequestDto planRequestDto) {
-        PlanResponseDto createdPlan = planService.createPlan(userId, planRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlan);
+    @GetMapping("/{planId}")
+    public ResponseEntity<MessageWithData> getPlan(@PathVariable Long planId) {
+//        Optional<Plan> plan = planService.getPlan(planId);
+//        return plan;
+        return null;
     }
 
-    @PutMapping("/{planId}")
-    public ResponseEntity<PlanResponseDto> updatePlan(@PathVariable UUID userId, @PathVariable Long planId, @RequestBody PlanRequestDto planRequestDto) {
-        PlanResponseDto updatedPlan = planService.updatePlan(userId, planId, planRequestDto);
-        return ResponseEntity.ok(updatedPlan);
+    @PostMapping("/{userId}")
+    public ResponseEntity<MessageWithData> createPlan(@PathVariable UUID userId, @RequestBody PlanRequestDto planRequestDto) {
+//        PlanResponseDto createdPlan = planService.createPlan(userId, planRequestDto);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlan);
+        return null;
+    }
+
+    @PutMapping("/{userId}/{planId}")
+    public ResponseEntity<MessageWithData> updatePlan(@PathVariable UUID userId, @PathVariable Long planId, @RequestBody PlanRequestDto planRequestDto) {
+//        PlanResponseDto updatedPlan = planService.updatePlan(userId, planId, planRequestDto);
+//        return ResponseEntity.ok(updatedPlan);
+        return null;
     }
 
     @DeleteMapping("/{planId}")
-    public ResponseEntity<Void> deletePlan(@PathVariable UUID userId, @PathVariable Long planId, @RequestBody PlanRequestDto planRequestDto) {
-        planService.deletePlan(userId, planId, planRequestDto);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<MessageWithData> deletePlan(@PathVariable UUID userId, @PathVariable Long planId, @RequestBody PlanRequestDto planRequestDto) {
+//        planService.deletePlan(userId, planId, planRequestDto);
+//        return ResponseEntity.noContent().build();
+        return null;
     }
 }
