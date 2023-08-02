29b64f7877f58c24d69881344b3dc82ab2d74d06
novventa <novventa@kakao.com>
2023-08-02 17:26:18 +0900

    :bulb: PlanRepository 및 PlanServiceImpl 주석 추가 주석 추가 Resolves #69

--- src/main/java/com/b5f1/atention/domain/plan/repository/PlanRepository.java
+++ src/main/java/com/b5f1/atention/domain/plan/repository/PlanRepository.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
-    // teamId로 해당 팀의 모든 일정 가져오기
+    // teamId로 해당 팀의 모든 일정 찾기
     List<Plan> findAllByTeamIdAndIsDeletedFalse(Long teamId);
-
+    // id로 일정 하나 찾기
     Optional<Plan> findByIdAndIsDeletedFalse(Long id);
-
+    // teamId 들로 모든 일정 찾기
     List<Plan> findAllByTeamIdInAndIsDeletedFalse(List<Long> teamIds);
 }
