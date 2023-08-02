eb4fd7cb3573431e669d2d34ce816e33bfe128f8
김기정 <cjdfidrlwjd@naver.com>
2023-08-02 12:34:21 +0900

    :sparkles: /user/login URL 요청은 인증/인가 filter를 수행하지 않고 통과하도록 구현 인증/인가 filter 진입 시 가장 먼저 request header에서 refreshToken을 추출 refreshToken이 있으면 유효성 검사 후 access/refreshToken 재발급 refreshToken이 없으면 accessToken 유효성 검사 후 Authenticaton 인증 정보 저장 Resolves #62

---
+++ src/main/java/com/b5f1/atention/domain/auth/jwt/JwtAuthenticationProcessingFilter.java
@@ -0,0 +1,97 @@
+package com.b5f1.atention.domain.auth.jwt;
+
+import com.b5f1.atention.domain.user.repository.UserRepository;
+import com.b5f1.atention.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 기본적으로 사용자는 요청 헤더에 AccessToken만 담아서 요청
+ * AccessToken 만료 시에만 RefreshToken을 요청 헤더에 AccessToken과 함께 요청
+ *
+ * 1. RefreshToken이 없고, AccessToken이 유효한 경우 -> 인증 성공 처리, RefreshToken을 재발급하지는 않는다.
+ * 2. RefreshToken이 없고, AccessToken이 없거나 유효하지 않은 경우 -> 인증 실패 처리, 403 ERROR
+ * 3. RefreshToken이 있는 경우 -> DB의 RefreshToken과 비교하여 일치하면 AccessToken 재발급, RefreshToken 재발급(RTR 방식)
+ *                              인증 성공 처리는 하지 않고 실패 처리
+ *
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
+
+    private static final String NO_CHECK_LOGIN_URL = "/user/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // "/login" 요청이 들어오면, 인증/인가 처리할 필요 없으므로 다음 필터 호출
+        if (request.getRequestURI().equals(NO_CHECK_LOGIN_URL)) {
+            filterChain.doFilter(request, response);
+            // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
+            return;
+        }
+
+        //가장 먼저 사용자 요청 헤더에서 refreshToken 추출
+        //refreshToken이 없거나 유효하지 않은 경우 null
+        String refreshToken = jwtService.extractRefreshToken(request)
+//              .filter(token -> jwtService.isTokenValid(token)) 이 람다식을 아래와 같이 메서드 참조 방식으로 작성 가능
+                //filter는 Optional 객체의 값을 조건에 따라 걸러냄. 여기서는 token이 유효하지 않으면 걸러냄
+                .filter(jwtService::isTokenValid)
+                //token이 유효하지 않아 걸려졌을 때 대채값으로 null을 반환
+                .orElse(null);
+
+        // 리프레시 토큰이 요청 헤더에 존재했다면, 사용자가 AccessToken이 만료되어서
+        // RefreshToken까지 보낸 것이므로 리프레시 토큰이 DB의 리프레시 토큰과 일치하는지 판단 후,
+        // 일치한다면 AccessToken을 재발급해준다.
+        if (refreshToken != null) {
+            reIssueRefreshTokenAndAccessToken(response, refreshToken);
+            /*
+            * RefreshToken을 보낸 경우에는 AccessToken을 재발급 하고
+            * 인증 처리는 하지 않게 하기위해 바로 return으로 필터 진행 막기
+            * */
+            return;
+        }
+
+        // RefreshToken이 없거나 유효하지 않다면, AccessToken을 검사하고 인증을 처리하는 로직 수행
+        // AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
+        // AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
+        else {
+            checkAccessTokenAndAuthentication(request, response, filterChain);
+        }
+    }
+
+    public void reIssueRefreshTokenAndAccessToken(HttpServletResponse response, String refreshToken) {
+
+    }
+
+    private String reIssueRefreshToken(User user) {
+        return null;
+    }
+
+    //액세스 토큰 체크 & 인증 처리 메소드
+    public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                                  FilterChain filterChain) throws ServletException, IOException {
+    }
+
+    public void saveAuthentication(User myUser) {
+    }
+}
